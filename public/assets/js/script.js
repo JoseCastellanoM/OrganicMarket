400043fda955d59af4eec3ca937973170ef28123
Jose Castellano <u20201b858@upc.edu.pe>
2022-06-24 21:57:10 -0500

    feat(subscription): Added content for monthly subscription

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,3 +2,7 @@ function sendEmail() {
     alert("¡Correo enviado!");
     top.location.href = "about.html";
 }
+function msgSubscription() {
+    alert("¡Gracias por suscribirte!");
+    top.location.href = "plans.html";
+}
